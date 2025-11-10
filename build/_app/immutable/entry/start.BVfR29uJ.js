d9ceb8e0c49bf6dfeed0aa19f1ed9e1d69604434
Timur Gabaidulin <timur.gab@gmail.com>
2025-11-10 09:28:44 -0500

    Fix vite base path for GitHub Pages

---
+++ build/_app/immutable/entry/start.BVfR29uJ.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/DARvm8eP.js";export{o as load_css,r as start};
--- build/_app/immutable/entry/start.VpxwngRf.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/BQO0D9JY.js";export{o as load_css,r as start};
